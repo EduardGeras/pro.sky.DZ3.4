4ce8087f39e3ff169679b817e3d356c090bb0e1d
EduardGeras <103679703+EduardGeras@users.noreply.github.com>
2023-01-17 18:43:40 +0500

    Initial commit

---
+++ src/main/java/com/example/pro_sky_dz3_4/Application.java
@@ -0,0 +1,13 @@
+package com.example.pro_sky_dz3_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
