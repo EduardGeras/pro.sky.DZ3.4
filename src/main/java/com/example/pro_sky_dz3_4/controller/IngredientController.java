f6475bc3c412fcd67c562be01fe8b75324cb7a59
EduardGeras <103679703+EduardGeras@users.noreply.github.com>
2023-01-17 21:50:57 +0500

    Домашка 3.4

---
+++ src/main/java/com/example/pro_sky_dz3_4/controller/IngredientController.java
@@ -0,0 +1,32 @@
+package com.example.pro_sky_dz3_4.controller;
+
+import com.example.pro_sky_dz3_4.model.Ingredient;
+import com.example.pro_sky_dz3_4.service.impl.IngredientServiceImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/ingredient")
+
+public class IngredientController {
+
+    private final IngredientServiceImpl ingredientService;
+
+    public IngredientController(IngredientServiceImpl ingredientService) {
+        this.ingredientService = ingredientService;
+    }
+
+    @PostMapping
+    public Ingredient createIngredient(@RequestBody Ingredient ingredient) {
+        return ingredientService.addIngredient(ingredient);
+    }
+
+    @GetMapping("/id")
+    public ResponseEntity<Ingredient> getIngredientInfo(@PathVariable Long id) {
+        Ingredient ingredient = ingredientService.findIngredient(id);
+        if (ingredient == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(ingredient);
+    }
+}
